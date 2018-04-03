1e5a465074b4ba0a63edaa492159bbe5b5e691d7
Stephen DeLorme <stephen@stephendelorme.com>
2018-04-03 11:53:55 -0400

    Update centered-vertical-leading-animation.jsx
    Added a note that the text layer needs to start at zero -- I encountered problems where keyframing does not work properly if the layer doesn't start at zero. Probably has to do with the fact that `time` is used as a variable in this expression.

--- centered-vertical-leading-animation.jsx
+++ centered-vertical-leading-animation.jsx
@@ -1,12 +1,13 @@
 /***************************
 This expression keeps your text anchor point centered vertically as you animate the line spacing (leading) of your text layer.
 Instructions
-1) Create text layer. Add an animator.
+1) Create text layer at 0;00;00;00. This is important! If it's not a 0, keyframing will not work properly.
+2) Add an animator.
 3) Add a "Slider Control" effect to your layer. Rename it to "Text Vertical Offset".
-3) Paste this expression onto your text layer's anchor point transform property.
-4) Adjust the "Text Vertical Offset" slider until the anchor point seems to be in the vertical center of your text.
-5) Now you can animate the line spacing of the text from the center!
-6) Note: if you adjust font size or other properties, you may need to re-adjust the "Text Vertical Offset".
+4) Paste this expression onto your text layer's anchor point transform property.
+5) Adjust the "Text Vertical Offset" slider until the anchor point seems to be in the vertical center of your text.
+6) Now you can animate the line spacing of the text from the center!
+7) Note: if you adjust font size or other properties, you may need to re-adjust the "Text Vertical Offset".
 ***************************/
 
 a=transform.anchorPoint;
